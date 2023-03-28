3f7adf3e585a1c5c07b108f4b7c1520602fd91f1
saurav singh <sinobeesaurav@gmail.com>
2023-03-28 18:58:01 +0530

    same change

---
+++ component/featureSection/Card.js
@@ -0,0 +1,8 @@
+function Card(){
+    return (
+        <div style={{width:"100%",height:367,backgroundColor:"cyan",marginTop:10}}>
+            <h1>Soham</h1>
+        </div>
+    )
+}
+export default Card;
