42f20018a5aa8f85c4500d818b45a37214324ca1
saurav singh <sinobeesaurav@gmail.com>
2023-03-28 18:57:06 +0530

    same change

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@/component/header/Header';
 import Filter from '@/component/filter/Filter';
-import Card from '@/component/futureSection/Card';
+import Card from '@/component/featureSection/Card';
 export default function Home() {
   return (
     <>
