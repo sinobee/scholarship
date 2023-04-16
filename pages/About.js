feaf73108eb6b629b1a51f1b2ab819dd1095aa6a
Sohamsinh <dabhisohamsinh1044@gmail.com>
2023-04-16 13:26:57 +0530

    Header Footer Nav Pages

--- pages/About.js
+++ pages/About.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import footerlink  from '@/component/header/Footerlink'
+import Footerlink from '@/component/header/Footerlink';
+
 
 const About = () => {
   return (
     <>
-    <footerlink />
     <div><h1>About Us</h1>
     <h2>This Page Includes Information About Scolarship Website main Goal And How to Use this Web Site and Other info Related to Scholarship Guidence  </h2>
     <h3>We can Add details about BarabariProject what are our goals info about our ngo </h3>
     <p>For More Information click here https://barabariproject.org/ </p>
     </div>
     
+    <br>
+    </br>
+    <br>  
+    </br>
+    <br>  
+    </br>
+    <br>  
+    </br>
+    <Footerlink />
     </>
+
   )
 }
 
--- pages/Contact.js
+++ pages/Contact.js
@@ -2,8 +2,72 @@ import React from 'react'
 
 const Conatct = () => {
   return (
-    <div>
-    <h1>Contact Us</h1>
+    
+  <div>
+    <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://bootswatch.com/5/sandstone/bootstrap.min.css"
+    />
+  
+
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <h1 class="mb-5">Contact Us</h1>
+          <form
+            action=""
+            method="POST"
+          >
+            <div class="mb-3">
+              <label class="form-label">Full Name</label>
+              <input name="name" type="text" class="form-control" required />
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">Email address</label>
+              <input name="email" type="email" class="form-control" required />
+              <div id="emailHelp" class="form-text">
+                Don't worry, we won't share it with anyone else.
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">Phone (optional)</label>
+              <input name="phone" type="tel" class="form-control" />
+              <div id="phoneHelp" class="form-text">
+                If you rather talk to a human directly.
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">Your Message</label>
+              <textarea
+                name="message"
+                class="form-control"
+                rows="3"
+                required
+              ></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+
+        <div class="col-1"></div>
+
+        <div class="col-6 position-relative">
+          <img
+            src="mail.svg"
+            class="w-75 position-absolute top-50 start-50 translate-middle"
+          />
+        </div>
+      </div>
+   
+
+    
+ 
+    </div>
     </div>
   )
 }
