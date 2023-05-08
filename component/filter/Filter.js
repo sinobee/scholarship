663a377f08a305361116dc8ea79bb4525a0f29ff
saurav singh <sinobeesaurav@gmail.com>
2023-05-08 17:03:30 +0530

    Filter work

--- component/filter/Filter.js
+++ component/filter/Filter.js
@@ -1,28 +1,63 @@
 import style from "../../styles/Filter.module.css"
 import Select from 'react-select';
-import { useState } from "react";
+import data from "../cardSection/data.json";
+import { useEffect, useState } from "react";
 import options from "./state.json";
 import ResultCard from "./ResultCard";
 function Filter(){
-    const [state ,setState] = useState("States..");
-
-    
+    const [state ,setState] = useState(data);
+    const [selectedState,setSelectedState]=useState([]);
     function handleState({label,value}){
-        console.log(value   )
-        setState(value)
+        const filterState = state?.filter((scholarship) => scholarship.state === value);
+        console.log(filterState);
+        setSelectedState(filterState);
     }
 
     function handleAge(e){
-        console.log(e.target.textContent);
+    //     const [selMin,selMax]= e.target.textContent.split('-');
+    //     console.log(selMin,selMax);
+    //    const filterage = selectedState?.filter((item)=>{
+    //         let agedata=null;
+    //         const [min, max] = item.age.split('-');
+    //         if(selMin>=min && max<=selMax){
+    //           agedata=item
+    //         }
+    //         return agedata;
+    //     })
+        // console.log(filterage);
+
     }
     function handleGender(e){
-        console.log(e.target.textContent);
+        const genderFilter= e.target.textContent;
+
+        const filteredGender = selectedState.filter(scholarship => {
+            return scholarship.gender.includes(genderFilter.toLowerCase());
+          });
+         setSelectedState(filteredGender)
     }
     function handleCast(e){
-        console.log(e.target.textContent);
+        const selectedCast = e.target.textContent;
+        const filterCast = selectedState.filter(scholarship => scholarship.cast.includes(selectedCast));
+        setSelectedState(filterCast)
     }
     function handleIncome(e){
-        console.log(e.target.textContent);
+        const selectedIncome = e.target.textContent;
+        const filteredIncome =selectedState.filter(item => {
+            switch (selectedIncome) {
+              case "10K-1L":
+                return item.income >= 10000 && item.income <= 100000;
+              case "1L-3L":
+                return item.incomes >= 100000 && item.income <= 300000;
+              case "4L-6L":
+                return item.income >= 400000 && item.income <= 600000;
+              case "7L-9L":
+                return item.income >= 700000 && item.income <= 900000;
+              case ">10L":
+                return item.income > 1000000;
+            }
+          });
+
+          setSelectedState(filteredIncome);
     }
 
     return(
@@ -69,7 +104,7 @@ function Filter(){
                              <label for="female" onClick={(e)=>handleGender(e)}>Female</label>
 
                             <input type={"radio"} name={"gender"}  id="tran"  />
-                            <label for="tran" onClick={(e)=>handleGender(e)}>Tran</label>
+                            <label for="tran" onClick={(e)=>handleGender(e)}>Other</label>
                            </div>
                         </div>
                         <div><p><b><i>Cast</i></b></p>
@@ -77,7 +112,7 @@ function Filter(){
                         <div className={`${style.cast} ${style.common}`}>
 
                             <input type={"radio"} name={"cast"}  id="general"  />
-                            <label for="general" onClick={(e)=>handleCast(e)}>General</label>
+                            <label for="general" onClick={(e)=>handleCast(e)}>Gen</label>
 
                             <input type={"radio"} name={"cast"}  id="OBC"  />
                             <label for="OBC" onClick={(e)=>handleCast(e)}>OBC</label>
